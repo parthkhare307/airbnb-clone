60d9cb47baa9e056873a3d3435579145886a142e
Parth Khare <parthkhare307@gmail.com>
2023-04-20 22:52:41 +0530

    Github Login

--- app/components/models/RegisterModel.tsx
+++ app/components/models/RegisterModel.tsx
@@ -17,6 +17,7 @@ import Heading from "../Heading";
 import Input from "../inputs/Input";
 import {toast } from 'react-hot-toast';
 import Button from "../Button";
+import { signIn } from "next-auth/react";
 
 const RegisterModel = () => {
 
@@ -100,7 +101,7 @@ const footerContent = (
       outline
       label="Continue with Github"
       icon={AiFillGithub}
-      onClick={() => {}}
+      onClick={() => signIn('github')}
     />
     <div
       className="
